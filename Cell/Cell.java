fee59ea164cbe675b221876244c4c44ee6c1e8de
sadrabera <s.berangi@mail.sbu.ac.ir>
2022-03-20 17:54:00 +0330

    abstract build method added

--- Cell/Cell.java
+++ Cell/Cell.java
@@ -1,8 +1,13 @@
 package Cell;
+
+import Cell.Purchasable.Land;
+import Player.Player;
+
 public abstract class Cell {
     public int location;
 
     public Cell(int location) {
         this.location = location;
     }
+    public abstract void build(Player player);
 }
