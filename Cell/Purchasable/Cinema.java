2949414df4fe4598be45b8e0dd087c32635585b5
sadrabera <s.berangi@mail.sbu.ac.ir>
2022-03-22 03:07:28 +0430

    methods of cinema has been created

--- Cell/Purchasable/Cinema.java
+++ Cell/Purchasable/Cinema.java
@@ -1,7 +1,25 @@
 package Cell.Purchasable;
 
+
+import Player.PlayGround;
+
 public class Cinema extends Purchasable{
     public Cinema(int location, double value, Color color) {
         super(location, value, color);
+    }
+    public int getTicket(){
+       int result=25;
+       result*=cinemaCounter()*2;
+       return result;
+    }
+    public int cinemaCounter(){
+        int result=0;
+        PlayGround map=PlayGround.getMap();
+        for(int i=0;i<map.cells.length;i++){
+            if(map.cells[i] instanceof Cinema)
+                if (((Cinema) map.cells[i]).owner.equals(this.owner))
+                    result++;
+        }
+        return result;
     }
 }
