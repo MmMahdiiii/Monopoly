ae3d99ea1eaaaa464204594d2824de4a865a3ef5
sadrabera <s.berangi@mail.sbu.ac.ir>
2022-03-22 17:30:19 +0430

    Tax created

--- Cell/BankOwned/Tax.java
+++ Cell/BankOwned/Tax.java
@@ -1,7 +1,13 @@
 package Cell.BankOwned;
 
+import Player.Player;
+
+
 public class Tax extends BankOwned {
     public Tax(int loc) {
         super(loc);
+    }
+    public boolean takeTax(Player player) {
+        return player.payment(player.money * 10 / 100);
     }
 }
