6a38c602fbae713a6d453cd397658adcd6d6303b
sadrabera <s.berangi@mail.sbu.ac.ir>
2022-03-24 01:57:24 +0430

    Packages name became lowerCase

--- player/Bonus.java
+++ player/Bonus.java
@@ -1,4 +1,4 @@
-package Player;
+package player;
 
 public enum Bonus {
     escapeFromDungeon,
--- player/PlayGround.java
+++ player/PlayGround.java
@@ -1,10 +1,10 @@
-package Player;
+package player;
 
-import cell.BankOwned.Parcking;
+import cell.bankOwned.Parcking;
 import cell.Cell;
-import cell.BankOwned.*;
-import cell.Purchasable.*;
-import cell.Purchasable.Land;
+import cell.bankOwned.*;
+import cell.purchasable.*;
+import cell.purchasable.Land;
 
 public class PlayGround {
     static PlayGround map=new PlayGround();
